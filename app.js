5c2d760ec55ef082bf09598b40cacaad35e841bc
Kevin <kevindlee98@gmail.com>
2020-10-23 18:30:38 -0700

    variable port

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ app.get('/', (req, res) => {
     res.send("Welcome to the HOMEPAGE");
 });
 
-app.listen(3000, () => {
+const port = process.env.port ||  3000;
+app.listen(port, () => {
     console.log("Kevin says Hello :)");
 });
 
